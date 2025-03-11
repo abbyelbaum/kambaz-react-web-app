82c4d637aa337e9e1c54417d99add116ab9246d8
abbyelbaum <abby.elbaum@gmail.com>
2025-03-10 23:07:10 -0400

    Modules

--- src/Kambaz/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kambaz/Courses/Modules/ModuleControlButtons.tsx
@@ -1,10 +1,20 @@
 import { BsPlus } from "react-icons/bs";
 import GreenCheckmark from "./GreenCheckmark";
 import { IoEllipsisVertical } from "react-icons/io5";
+import { FaTrash } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
 
-export default function ModuleControlButtons() {
+export default function ModuleControlButtons(
+    {moduleId, deleteModule, editModule}: {
+        moduleId: string; 
+        deleteModule: (moduleId: string) => void;
+        editModule: (moduleId: string) => void;
+    }
+) {
     return(
         <div className="float-end">
+            <FaPencil className="text-primary me-3" onClick={() => editModule(moduleId)} />
+            <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)} />
             <GreenCheckmark/>
             <BsPlus className="fs-2"/>
             <IoEllipsisVertical className="fs-4"/>
